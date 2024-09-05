a7069ff2542f5e3552836e5f46062f16bbb7074c
anilkumararudra <anilkumararudra@gmail.com>
2024-09-05 23:22:39 +0530

    Update testvars.sh

--- scripts/testvars.sh
+++ scripts/testvars.sh
@@ -5,10 +5,8 @@ for var in `cat Tempfile`
 do
   name=`echo "$var" | awk -F: '{print $1}'`
   value=`echo "$var" | awk '-F: ' '{print $2}'`
-  echo $name
-  echo $value
-  #sed 's/${$name}/$value/g' test.tomb >> outuput1.txt
-  #sed 's/${{ $name }}/$value/g' test.tomb >> output2.txt
+  sed -i 's/${$name}/$value/g' test.tomb
+  sed -i 's/${{ $name }}/$value/g' test.tomb
 done
 # cat output1.txt
 # cat output2.txt
