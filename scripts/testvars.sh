d6deba2c80745763dc637583b8b066a8d6d34633
anilkumararudra <anilkumararudra@gmail.com>
2024-09-06 19:22:47 +0530

    Update testvars.sh

--- scripts/testvars.sh
+++ scripts/testvars.sh
@@ -3,14 +3,15 @@ echo "${ALLMYVARS:1:-1}" | sed "s/\"//g" | sed 's/,$//' | sed '/^[[:space:]]*$/d
 echo "${INPUTVARS:1:-1}" | sed "s/\"//g" | sed 's/,$//' | sed '/^[[:space:]]*$/d' >> Tempfile
 while read line
 do
-    printf "%s\n" "$line"
+    #printf "%s\n" "$line"
     var_name=`echo "$line" | awk -F: '{print $1}'`
     var_value=`echo "$line" | awk '-F: ' '{print $2}'`
     tempvar1=`echo "$var_name" | awk '{print "${"$0"}"}'`
     tempvar2=`echo "$var_name" | awk '{print "${{ "$0" }}"}'`
     tempvar3=`echo "$var_name" | awk "$"$0""}'`
+    echo "$tempvar3"
     sed -i "s,$tempvar1,$var_value,g" $1
     sed -i "s,$tempvar2,$var_value,g" $1
     sed -i "s,$tempvar3,$var_value,g" $1
-    head -5 $1
+    head -10 $1
 done < "Tempfile"
