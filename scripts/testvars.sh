85bd1b8a7ff7e6bfebb6b991eb0419386ed1b110
anilkumararudra <anilkumararudra@gmail.com>
2024-09-06 12:17:25 +0530

    Update testvars.sh

--- scripts/testvars.sh
+++ scripts/testvars.sh
@@ -5,11 +5,11 @@ cat Tempfile
 for var in `cat Tempfile`
 do
   echo "$var"
-  var_name=`echo "$var" | awk -F: '{print $1}'`
-  var_value=`echo "$var" | awk '-F: ' '{print $2}'`
-  echo "newvar=$($var_name)"
-  sed -i 's/${name}/$value/g' test.tomb
-  sed -i 's/${{ name }}/$value/g' test.tomb
+  #var_name=`echo "$var" | awk -F: '{print $1}'`
+  #var_value=`echo "$var" | awk '-F: ' '{print $2}'`
+  #echo "newvar=$($var_name)"
+  #sed -i 's/${name}/$value/g' test.tomb
+  #sed -i 's/${{ name }}/$value/g' test.tomb
 done
 # cat output1.txt
 # cat output2.txt
