71a37b1f4ff0fad283b1c6e3b4be7491576c31fd
anilkumararudra <anilkumararudra@gmail.com>
2024-09-06 15:54:27 +0530

    Update testvars.sh

--- scripts/testvars.sh
+++ scripts/testvars.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "${ALLMYVARS:1:-1}" | sed "s/\"//g" | sed 's/,$//' | sed '/^[[:space:]]*$/d' > Tempfile
+echo "${INPUTVARS:1:-1}" | sed "s/\"//g" | sed 's/,$//' | sed '/^[[:space:]]*$/d' >> Tempfile
 while read line
 do
     printf "%s\n" "$line"
