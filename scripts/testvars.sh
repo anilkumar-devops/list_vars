c646ae2939881cff2f20a870693069018b2eb702
anilkumararudra <anilkumararudra@gmail.com>
2024-09-06 13:08:17 +0530

    Update testvars.sh

--- scripts/testvars.sh
+++ scripts/testvars.sh
@@ -28,7 +28,7 @@ do
     #sed -i "s/'$tempvar1'/$var_value/g" ./test.tomb
     #sed -i "s/'$tempvar2'/$var_value/g" ./test.tomb
     head -5 test.tomb
-    sed -e "s,$tempvar1,$var_value,g" -i test.tomb
+    sed -i "s,$tempvar1,$var_value,g" test.tomb
 done < "Tempfile"
 # cat output1.txt
 # cat output2.txt
