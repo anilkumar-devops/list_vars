08929ff0177a59a8c69a2844b9019e9a6a14abb4
anilkumararudra <anilkumararudra@gmail.com>
2024-09-06 12:24:40 +0530

    Update testvars.sh

--- scripts/testvars.sh
+++ scripts/testvars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "$ALLMYVARS" ${s:1:-1}
 echo "${ALLMYVARS:1:-1}" | sed "s/\"//g" | sed 's/,$//' > Tempfile
-cat Tempfile
+head -2 Tempfile
 for var in `cat Tempfile`
 do
   echo "$var"
